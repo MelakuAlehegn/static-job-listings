12dc708c94d03c87050045c399dd240d8148ed45
unknown <melakualehegn34@gmail.com>
2023-08-23 02:22:25 +0300

    add theme color and screens to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {},
+    screens: {
+      sm: '375px',
+      md: '480px',
+      lg: '768px'
+    },
+    color: {
+      desaturatedDarkCyan: 'hsl(180, 29%, 50%)',
+      lightGreyishCyanBg: 'hsl(180, 52%, 96%)',
+      lightGrayishCyanFilter: 'hsl(180, 31%, 95%)',
+      darkGrayishCyan: 'hsl(180, 8%, 52%)',
+      veryDarkGrayishCyan: 'hsl(180, 14%, 20%)'
+    },
+    fontFamily: {
+      sans: ['League Spartan', "sans-serif"]
+    }
   },
   plugins: [],
 }
